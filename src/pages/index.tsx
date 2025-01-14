cc844b8645a9bd6fb146c0bf22ac41f70f3eeb74
felprangel <feliperangel897@gmail.com>
2025-01-13 23:08:55 -0300

    adiciona botão

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,29 @@
 import { Header } from '@/components/Header'
+import styled from 'styled-components'
 
 export default function Index() {
-  return <Header />
+  return (
+    <>
+      <Header />
+      <Button>+ Adicionar Livro</Button>
+    </>
+  )
 }
+
+const Button = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 0;
+  margin: 1.5em 0;
+  font-size: 1.4em;
+  font-weight: 600;
+  padding: 0.7em 1em;
+  border-radius: 0.7em;
+  transition: 150ms linear;
+  outline: none;
+
+  &:hover {
+    background-color: var(--dark-gray);
+  }
+`
