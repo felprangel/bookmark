4f9ee53ac3d502e070212461209128dc359cdd03
felprangel <feliperangel897@gmail.com>
2025-01-13 22:23:09 -0300

    adiciona componente de header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export function Header() {
+  return (
+    <StyledHeader>
+      <Heading>Bookmark</Heading>
+    </StyledHeader>
+  )
+}
+
+const StyledHeader = styled.header`
+  background-color: var(--white);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 7em;
+`
+
+const Heading = styled.h1`
+  font-size: 2.5em;
+`
