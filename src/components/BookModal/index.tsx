980a73677e46434066b55d729ea3cf2a79b81109
felprangel <feliperangel897@gmail.com>
2025-01-19 08:26:05 -0300

    adicona esqueleto do modal

--- src/components/BookModal/index.tsx
+++ src/components/BookModal/index.tsx
@@ -1,1 +1,13 @@
-export function BookModal() {}
+import { Modal } from '@mui/material'
+
+interface ModalProps {
+  open: boolean
+}
+
+export function BookModal(props: ModalProps) {
+  return (
+    <Modal open={props.open}>
+      <h1>Teste</h1>
+    </Modal>
+  )
+}
