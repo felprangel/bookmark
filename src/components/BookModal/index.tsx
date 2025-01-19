43f0dc4dc661c2ba0605a107361da773f4ef7302
felprangel <feliperangel897@gmail.com>
2025-01-19 08:46:33 -0300

    altera name dos inputs

--- src/components/BookModal/index.tsx
+++ src/components/BookModal/index.tsx
@@ -10,12 +10,12 @@ export function BookModal(props: ModalProps) {
     <Dialog open={props.open} onClose={props.onClose}>
       <form>
         <h1>Adicionar Livro</h1>
-        <input type="text" name="book_title" placeholder="Título" maxLength={100} required />
-        <input type="text" name="book_author" placeholder="Autor" maxLength={100} required />
-        <input type="number" name="book_pages" placeholder="Páginas" min={0} max={10000} required />
+        <input type="text" name="title" placeholder="Título" maxLength={100} required />
+        <input type="text" name="author" placeholder="Autor" maxLength={100} required />
+        <input type="number" name="pages" placeholder="Páginas" min={0} max={10000} required />
         <div>
           <h2>Lido?</h2>
-          <input type="checkbox" name="book_status" />
+          <input type="checkbox" name="status" />
         </div>
         <button id="done">Pronto!</button>
       </form>
