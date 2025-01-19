f7c9d82102f6093785aa634edf9fb7929b988f08
felprangel <feliperangel897@gmail.com>
2025-01-19 08:41:32 -0300

    adiciona onClose

--- src/components/BookModal/index.tsx
+++ src/components/BookModal/index.tsx
@@ -2,11 +2,12 @@ import { Dialog } from '@mui/material'
 
 interface ModalProps {
   open: boolean
+  onClose: () => void
 }
 
 export function BookModal(props: ModalProps) {
   return (
-    <Dialog open={props.open}>
+    <Dialog open={props.open} onClose={props.onClose}>
       <h1>Teste</h1>
     </Dialog>
   )
