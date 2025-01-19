38032ec197732566d718fd29a747ce71e1d959eb
felprangel <feliperangel897@gmail.com>
2025-01-19 08:45:51 -0300

    adiciona elementos do formulário

--- src/components/BookModal/index.tsx
+++ src/components/BookModal/index.tsx
@@ -8,7 +8,17 @@ interface ModalProps {
 export function BookModal(props: ModalProps) {
   return (
     <Dialog open={props.open} onClose={props.onClose}>
-      <h1>Teste</h1>
+      <form>
+        <h1>Adicionar Livro</h1>
+        <input type="text" name="book_title" placeholder="Título" maxLength={100} required />
+        <input type="text" name="book_author" placeholder="Autor" maxLength={100} required />
+        <input type="number" name="book_pages" placeholder="Páginas" min={0} max={10000} required />
+        <div>
+          <h2>Lido?</h2>
+          <input type="checkbox" name="book_status" />
+        </div>
+        <button id="done">Pronto!</button>
+      </form>
     </Dialog>
   )
 }
