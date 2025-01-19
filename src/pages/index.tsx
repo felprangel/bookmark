e88c47d1ba89401c101cf3b583fba830e76379ad
felprangel <feliperangel897@gmail.com>
2025-01-19 09:09:16 -0300

    adicoina imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
+import { BookModal } from '@/components/BookModal'
 import { Header } from '@/components/Header'
+import { useState } from 'react'
 import styled from 'styled-components'
 
 export default function Index() {
