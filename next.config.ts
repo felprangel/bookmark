7e4351006a3982d2e18702c59583bd083b1bd695
felprangel <feliperangel897@gmail.com>
2025-03-09 16:52:10 -0300

    adiciona env no next config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-};
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.BACKEND_URL
+  }
+}
 
-export default nextConfig;
+export default nextConfig
