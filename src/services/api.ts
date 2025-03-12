7e17f204ccc8ca431f9e9bddb7baa3eef2f667c5
felprangel <feliperangel897@gmail.com>
2025-03-12 19:25:05 -0300

    adiciona interceptadores de request e resposta

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,3 +1,27 @@
+import { COOKIE_TOKEN } from '@/database/local'
 import axios from 'axios'
+import Cookie from 'js-cookie'
 
 export const api = axios.create({ baseURL: process.env.NEXT_PUBLIC_API_URL })
+
+api.interceptors.request.use(
+  request => {
+    const token = Cookie.get(COOKIE_TOKEN)
+    if (token) {
+      request.headers.Authorization = `Bearer ${token}`
+    }
+    return request
+  },
+  error => Promise.reject(error)
+)
+
+api.interceptors.response.use(
+  response => response,
+  error => {
+    if (Number(error.response?.status) === 401) {
+      Cookie.remove(COOKIE_TOKEN)
+      window.location.href = '/login'
+    }
+    Promise.reject(error)
+  }
+)
