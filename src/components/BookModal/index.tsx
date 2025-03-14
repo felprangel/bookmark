1ee4e88965ae20825f071cb6d7dac8f2d3a13a1a
felprangel <feliperangel897@gmail.com>
2025-03-14 20:33:15 -0300

    adiciona exibição dos erros no formulário

--- src/components/BookModal/index.tsx
+++ src/components/BookModal/index.tsx
@@ -3,6 +3,7 @@ import { Checkbox, Dialog } from '@mui/material'
 import { useFormik } from 'formik'
 import styled from 'styled-components'
 import { z } from 'zod'
+import { toFormikValidationSchema } from 'zod-formik-adapter'
 
 interface ModalProps {
   open: boolean
@@ -20,6 +21,16 @@ export interface BookProps {
 type BookForm = Omit<BookProps, 'id'>
 
 export function BookModal(props: ModalProps) {
+  const bookSchema = z.object({
+    title: z.string({ required_error: 'O título é obrigatório' }),
+    author: z.string({ required_error: 'O autor é obrigatório' }),
+    pages: z
+      .number()
+      .min(0, 'O número de paǵinas deve ser maior do que 0')
+      .max(10000, 'O número de páginas deve ser maior do que 10000'),
+    read: z.boolean()
+  })
+
   const formik = useFormik({
     initialValues: {
       title: '',
@@ -27,17 +38,12 @@ export function BookModal(props: ModalProps) {
       pages: 0,
       read: false
     },
-    onSubmit: handleSubmit
+    validationSchema: toFormikValidationSchema(bookSchema),
+    onSubmit: handleSubmit,
+    validateOnBlur: true
   })
 
   async function handleSubmit(data: BookForm) {
-    z.object({
-      title: z.string().nonempty('O título é obrigatório'),
-      author: z.string().nonempty('O autor é obrigatório'),
-      pages: z.number().min(0, 'O número de paǵinas deve ser maior do que 0'),
-      read: z.boolean()
-    }).parse(data)
-
     await api.post('/books', data)
 
     window.dispatchEvent(new Event('storage'))
@@ -48,42 +54,46 @@ export function BookModal(props: ModalProps) {
     <Dialog open={props.open} onClose={props.onClose}>
       <StyledForm onSubmit={formik.handleSubmit}>
         <h1>Adicionar Livro</h1>
-        <StyledInput
-          type="text"
-          name="title"
-          placeholder="Título"
-          maxLength={100}
-          required
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.title}
-        />
-        <StyledInput
-          type="text"
-          name="author"
-          placeholder="Autor"
-          maxLength={100}
-          required
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.author}
-        />
-        <StyledInput
-          type="number"
-          name="pages"
-          placeholder="Páginas"
-          min={0}
-          max={10000}
-          required
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.pages}
-        />
+        <InputContainer>
+          <StyledInput
+            type="text"
+            name="title"
+            placeholder="Título"
+            maxLength={100}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.title}
+          />
+          {formik.touched.title && formik.errors.title && <ErrorMessage>{formik.errors.title}</ErrorMessage>}
+        </InputContainer>
+        <InputContainer>
+          <StyledInput
+            type="text"
+            name="author"
+            placeholder="Autor"
+            maxLength={100}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.author}
+          />
+          {formik.touched.author && formik.errors.author && <ErrorMessage>{formik.errors.author}</ErrorMessage>}
+        </InputContainer>
+        <InputContainer>
+          <StyledInput
+            type="number"
+            name="pages"
+            placeholder="Páginas"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.pages}
+          />
+          {formik.touched.pages && formik.errors.pages && <ErrorMessage>{formik.errors.pages}</ErrorMessage>}
+        </InputContainer>
         <CheckboxContainer>
           <h2>Lido?</h2>
           <Checkbox name="read" onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.read} />
         </CheckboxContainer>
-        <Button>Pronto!</Button>
+        <Button type="submit">Pronto!</Button>
       </StyledForm>
     </Dialog>
   )
@@ -96,16 +106,30 @@ const StyledForm = styled.form`
   margin: 2em;
 `
 
+const InputContainer = styled.div`
+  width: 100%;
+  max-width: 23em;
+  margin: 0.5em 0;
+  display: flex;
+  flex-direction: column;
+`
+
 const StyledInput = styled.input`
   border: none;
   background-color: #f0eef1;
   padding: 1.3em;
-  margin: 0.5em 0;
   border-radius: 0.4em;
-  width: 23em;
+  width: 100%;
   font-size: 1em;
   font-weight: 700;
   outline: none;
+`
+
+const ErrorMessage = styled.span`
+  color: #ff3333;
+  font-size: 0.8em;
+  margin-top: 0.3em;
+  align-self: flex-start;
 `
 
 const CheckboxContainer = styled.div`
