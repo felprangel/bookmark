700e660db4907d5cdf3cdac1bfbf698a25089f3d
felprangel <feliperangel897@gmail.com>
2025-04-03 20:23:28 -0300

    adiciona config de toast

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import '@/styles/globals.ts'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import { Toaster } from 'react-hot-toast'
 
 const queryClient = new QueryClient()
 
@@ -18,6 +19,7 @@ export default function App({ Component, pageProps }: AppProps) {
           <Component {...pageProps} />
         </AuthProvider>
         <GlobalStyle />
+        <Toaster />
       </QueryClientProvider>
     </>
   )
